887da7ebe8c819b2edde0e860ec5a36e7f388988
Adam Migodzinski <adam.migodzinski@goodylabs.com>
2020-05-19 12:19:58 +0200

    możliwość wgrywania wielu zdjęc dla wariantów

--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :virtual_path => 'spree/admin/images/index',
-  :replace => %q{code[erb-loud]:contains('t(:new_image)')},
-  :text => %q{<%= link_to_with_icon('icon-upload', t('multi_upload.title'), multi_upload_admin_product_images_path(@product), :id => 'multi_upload_images_link', :class => 'button', :remote => true) %>},
+  insert_after: 'erb:contains("content_for :page_actions do")',
+  :text => "<%= button_link_to t('multi_upload.title'), multi_upload_admin_product_images_path(@product), { :id => 'multi_upload_images_link', class: 'btn-warning btn-margined' } %>",
   :name => 'add_images_multi_upload_button'
 )
