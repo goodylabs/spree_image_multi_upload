113407fd6f43ccc090a7aa21c9eab51215c3927a
Adam Migodzinski <adam.migodzinski@goodylabs.com>
2020-05-19 08:32:29 +0200

    adjusting to 2.3.3 and fixing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Spree::Core::Engine.routes.draw do
   namespace :admin do
     resources :products do
       resources :images do
-        collection do
-          get 'multi_upload'
-        end
+        get :multi_upload, on: :collection
+        post :create_multiple, on: :collection
       end
     end
   end
